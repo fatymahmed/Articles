7ea4220d565994815413031bb8f40905c9db8bcc
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-04-14 12:05:40 +0300

    Mongoose and model setup

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/nodekb");
+let db = mongoose.connection;
+
+// Check connection
+db.once("open", function () {
+  console.log("Connected to mongodb");
+});
+
+// Check for DB errors
+db.on("error", function (err) {
+  console.log(err);
+});
 // Init App
 const app = express();
 
+// Bring in models
+let Articles = require("./models/article");
 // Load View Engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Home Route
 app.get("/", function (req, res) {
-  let articles = [
-    {
-      id: 1,
-      title: "Article One",
-      author: "Brad Traversy",
-      body: "This is article one",
-    },
-    {
-      id: 2,
-      title: "Article Two",
-      author: "John Doe",
-      body: "This is article two",
-    },
-  ];
-  res.render("index", {
-    title: "Articles",
-    articles: articles,
+  Articles.find({}, function (err, articles) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        title: "Articles",
+        articles: articles,
+      });
+    }
   });
-});
-
-// Add Route
-app.get("/articles/add", function (req, res) {
-  res.render("add", {
-    title: "Add article",
+}),
+  app.get("/articles/add", function (req, res) {
+    res.render("add", {
+      title: "Add article",
+    });
   });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000..");
